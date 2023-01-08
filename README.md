c4b863541b30a240c0ac7ed7902cf96836fd2812
Raunak Kedia <raunakkedia1@gmail.com>
2023-01-09 01:38:08 +0530

    initial commit -  setup bootstrap, routes using react-router-dom

---
+++ README.md
@@ -0,0 +1,6 @@
+# Notes APP using Vite + React + TS
+
+### Libraries/Tools used:
+1. React.ts using `Vite`
+2. react-router-dom
+3. bootstrap, react-bootstrap for styling
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const App = () => {
+	return (
+		<Container className="my-4">
+			<Routes>
+				<Route path="/" element={<h1>Home</h1>} />
+				<Route path="/new" element={<h1>New</h1>} />
+				<Route path="/:id">
+					<Route index element={<h1>Show</h1>} />
+					<Route path="edit" element={<h1>Edit</h1>} />
+				</Route>
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+		</Container>
+	);
+};
+
+export default App;
